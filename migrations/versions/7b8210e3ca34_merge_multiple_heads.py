bdd4f3e27b3810d947110c945f093090f6010d78
Goodness Aigbokhan <98638330+goodnessaig1@users.noreply.github.com>
2026-06-08 09:26:39 +0100

    Resolve alembic migration conflicts and merge heads

--- migrations/versions/7b8210e3ca34_merge_multiple_heads.py
+++ migrations/versions/7b8210e3ca34_merge_multiple_heads.py
@@ -1,8 +1,8 @@
-"""add user_type to partner_users
+"""Merge multiple heads
 
-Revision ID: 24cb22ba77b5
-Revises: 972a89ccdc8d
-Create Date: 2026-06-08 07:07:29.023973
+Revision ID: 7b8210e3ca34
+Revises: 2260212b8a0d, 24cb22ba77b5
+Create Date: 2026-06-08 09:21:48.879140
 
 """
 from typing import Sequence, Union
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '24cb22ba77b5'
-down_revision: Union[str, None] = '972a89ccdc8d'
+revision: str = '7b8210e3ca34'
+down_revision: Union[str, None] = ('2260212b8a0d', '24cb22ba77b5')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
